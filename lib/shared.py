3ea01c1cad62bbed885da3dd694fd5b4316078df
TimurKarevV <t.karev@faqtrip.com>
2024-02-14 15:58:47 +0700

    shared lib flow (for rebuild)

--- lib/shared.py
+++ lib/shared.py
@@ -1,2 +1,2 @@
 def print_error(context):
-    context.error("Shared")
+    context.error("Shared ")
--- test/src/main.py
+++ test/src/main.py
@@ -2,4 +2,5 @@ import os
 from common.shared import print_error
 
 def main(context):
+    context.error("Main ")
     print_error(context)
